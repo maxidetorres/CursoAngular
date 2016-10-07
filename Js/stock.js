3b5e291fa13a810b2c1d1099952c0b81b995e523
Maximiliano de torres <maxidetorres@gmail.com>
2016-10-06 21:26:47 -0300

    Se agrego add.html

--- Js/stock.js
+++ Js/stock.js
@@ -1,8 +1,8 @@
 angular.module('stockApp', [])
   .controller('stockController', function($scope) {
       $scope.productos=[
-          {codigo:'001', descripcion:'Futbol', precio: 20 , nota:'En oferta'},
-          {codigo:'002', descripcion:'Carro', precio: 3500, nota:'Novedad'},
-          {codigo:'003', descripcion:'Nokia 1100', precio: 35, nota:'Producto antiguo'},
+          {codigo:'001', descripcion:'Futbol', precio: 20 , nota:'En oferta',imagen:'001.jpg'},
+          {codigo:'002', descripcion:'Carro', precio: 3500, nota:'Novedad',imagen:'002.jpg'},
+          {codigo:'003', descripcion:'Nokia 1100', precio: 35, nota:'Producto antiguo',imagen:'003.jpg'},
       ];
 });
