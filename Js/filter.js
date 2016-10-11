9d47be6878d5c5394b8db015cf2cdcd30afe5fd6
maxidetorres <maxidetorres@gmail.com>
2016-10-11 20:09:53 -0300

    se agrego filtro

--- Js/filter.js
+++ Js/filter.js
@@ -1,1 +1,11 @@
-var app = angular.module('stockApp',[]);
+'use strict';
+var app = angular.module('stockApp');
+app.filter('filterCodigo', function(){
+    return function(input){
+        var valor="";
+        var prefijo= input.substr(0,2);
+        var subfijo= input.substr(2);
+        valor= prefijo +'-'+subfijo;
+        return valor; 
+    };  
+});
